04e0006f00f49ebeeed399b5eebb14000fea0f1d
kipsangtitus95@gmail.com <kipsangtitus95@gmail.com>
2018-07-05 20:19:12 +0300

    changes made

---
+++ CSnakeXenzia/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace CSnakeXenzia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitFrame();
+            Drawframe();
+            Console.ReadKey();
+           
+
+        }
+       
+        static char [][] grid=new char[20][];
+        
+        static int height=20;
+        static int width=50;
+
+        private static void InitFrame()
+        {
+            Console.CursorVisible = false;
+            for (int i = 0; i < height; i++)
+            {
+                grid[i] = new char[width];
+            }
+
+                grid[0][0] = '0';
+                grid[0][width - 1] = '0';
+                grid[height - 1][0] = '0';
+                grid[height - 1][width - 1] = '0';
+            for (int i =1; i < height-1; i++)
+            {
+                grid[i][0] = '|';
+                grid[i][width - 1] = '|';
+            }
+
+            for (int i = 1; i < width-1; i++)
+            {
+                grid[0][i] = '=';
+                grid[height - 1][i] = '=';
+            }
+
+            for (int k = 1; k < height-1; k++)
+            {
+                for (int c = 1; c <width-1; c++)
+                {
+                    grid[k][c] = ' ';
+                    
+                }  
+            }
+                
+            
+        }
+
+        public static void Drawframe()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.SetCursorPosition(x,y);
+                    Console.Write((grid[y][x]));
+                }
+            }
+        }
+    }
+    
+   
+}
